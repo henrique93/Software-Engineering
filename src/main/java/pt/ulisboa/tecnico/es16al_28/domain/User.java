6ae2a006b40863b2b05a40ef780984cf459a0d13
pedrocruz27 <pedro.m.g.cruz@tecnico.ulisboa.pt>
2016-03-13 18:50:38 +0000

    XML export compilagit status

--- src/main/java/pt/ulisboa/tecnico/es16al_28/domain/User.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/domain/User.java
@@ -1,5 +1,11 @@
 package pt.ulisboa.tecnico.es16al_28.domain;
 
+import org.jdom2.Element;
+
+/* Import exceptions */
+import java.io.UnsupportedEncodingException;
+import pt.ulisboa.tecnico.es16al_28.exception.ImportDocumentException;
+
 public class User extends User_Base {
     
     public User(String username, String password, String name, String umask) {
@@ -7,6 +13,10 @@ public class User extends User_Base {
         setPassword(password);
         setName(name);
         setUmask(umask);
+    }
+
+   public User(Element xml) throws ImportDocumentException, NotFileException {
+        xmlImport(xml);
     }
 
     /**
@@ -23,5 +33,20 @@ public class User extends User_Base {
     public String toString() {
         return "Name:" + getName() + "Username:" + getUsername() + "Umask:" + getUmask();
     }
-    
+	
+
+     public void xmlImport(Element userElement) throws ImportDocumentException, NotFileException {
+
+        try {
+            setUsername(new String(userElement.getAttribute("username").getValue().getBytes("UTF-8")));
+            setPassword(new String(userElement.getAttribute("password").getValue().getBytes("UTF-8")));
+            setName(new String(userElement.getAttribute("name").getValue().getBytes("UTF-8")));
+            setUmask(new String(userElement.getAttribute("umask").getValue().getBytes("UTF-8")));
+            
+        } catch (UnsupportedEncodingException e) {
+            throw new ImportDocumentException();
+        }
+       
+    }    
+
 }
