eeb8a4873004e9d8f757652b2b32793164cee3c5
gus-gomes <gustavo_gomes94@hotmail.com>
2016-03-14 20:05:54 +0000

    Update Dir.java
    debugging

--- src/main/java/pt/ulisboa/tecnico/es16al_28/domain/Dir.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/domain/Dir.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import java.util.Iterator;
 
 /* Import exceptions */
-import java.io.UnsupportedEncodingException;
 import org.jdom2.DataConversionException;
 import pt.ulisboa.tecnico.es16al_28.exception.ImportDocumentException;
 import pt.ulisboa.tecnico.es16al_28.exception.FileNotFoundException;
@@ -97,7 +96,7 @@ public class Dir extends Dir_Base {
             setPermission(new String(pfileElement.getAttribute("permission").getValue().getBytes("UTF-8")));
             setOwner(new String(pfileElement.getAttribute("owner").getValue().getBytes("UTF-8")));
 
-        } catch (UnsupportedEncodingException | DataConversionException e) {
+        } catch (DataConversionException e) {
             throw new ImportDocumentException();
         }
     }
