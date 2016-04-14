72281cffaf4e3b1149017257b5ebdd0ed558603f
henrique93 <henrique.lourenco093@gmail.com>
2016-04-14 14:59:34 +0100

    updated exceptions for log.warn

--- src/main/java/pt/ulisboa/tecnico/es16al_28/exception/TokenDoesNotExistException.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/exception/TokenDoesNotExistException.java
@@ -1,10 +1,16 @@
 package pt.ulisboa.tecnico.es16al_28.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class TokenDoesNotExistException extends MyDriveException {
     
     private static final long serialVersionUID = 1L;
-    
+
+    static final Logger log = LogManager.getRootLogger();
+
     public TokenDoesNotExistException() {
         super("This session does not exist");
+        log.warn("User of an inexistant token");
     }
 }
--- src/main/java/pt/ulisboa/tecnico/es16al_28/exception/TokenExpiredException.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/exception/TokenExpiredException.java
@@ -1,10 +1,15 @@
 package pt.ulisboa.tecnico.es16al_28.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class TokenExpiredException extends MyDriveException {
     
     private static final long serialVersionUID = 1L;
-    
+    static final Logger log = LogManager.getRootLogger();
+
     public TokenExpiredException() {
         super("Your session has expired. Please relog to continue operating");
+        log.warn("User of an expired token");
     }
 }
