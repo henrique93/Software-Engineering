4c27c1a690ccdd90e2f5f5d6bdee7a36f6b6a55d
Pedro Portugal <pedro_slb_minhoca@hotmail.com>
2016-04-14 23:09:47 +0100

    app, plain file, user modifications and comments

--- src/main/java/pt/ulisboa/tecnico/es16al_28/domain/App.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/domain/App.java
@@ -1,15 +1,45 @@
 package pt.ulisboa.tecnico.es16al_28.domain;
 
 import org.jdom2.Element;
+import java.io.UnsupportedEncodingException;
+
+import pt.ulisboa.tecnico.es16al_28.exception.ImportDocumentException;
 
 public class App extends App_Base {
     
-    public App(MyDrive mydrive, String name, String permission, User owner, Dir dir, String app) {
+    /**
+     *  Common App constructor
+     *  @param  login       Current MyDrive login
+     *  @param  name        App's name
+     *  @param  app         App's content
+     */
+    public App(Login login, String name, String app) {
         super();
-        init(mydrive, name, permission, owner, dir);
-	super.setApp(app);
+        init(login.getMydriveL(), name, login.getUser().getUmask(), login.getUser(), login.getCurrentDir());
+        setApp(app);
     }
     
+      /**
+     *  App's constructor for XML
+     *  @param  login       Current MyDrive login
+     *  @param  owner        App's owner
+     *  @param  dir         App's dir
+     *  @param  xml	xml element
+     */
+    public App(MyDrive mydrive, User owner, Dir dir, Element xml) throws ImportDocumentException {
+
+     	initxml(mydrive, owner, dir,  xml);	
+    }
+    
+
+
+    public void xmlImport(Element appElement) throws ImportDocumentException {
+
+		super.xmlImport(appElement);
+
+    }
+    
+   
     public Element xmlExport() {
         Element element = super.xmlExport();
 	element.setName("App");
--- src/main/java/pt/ulisboa/tecnico/es16al_28/domain/User.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/domain/User.java
@@ -11,59 +11,70 @@ import pt.ulisboa.tecnico.es16al_28.exception.ImportUserException;
 import pt.ulisboa.tecnico.es16al_28.exception.NotFileException;
 import pt.ulisboa.tecnico.es16al_28.exception.UserAlreadyExistsException;
 import pt.ulisboa.tecnico.es16al_28.exception.InvalidUsernameException;
+import pt.ulisboa.tecnico.es16al_28.exception.PermissionDeniedException;
 
 public class User extends User_Base {
     
-    public User(String username, String password, String name, String umask, MyDrive mydrive) throws UserAlreadyExistsException , InvalidUsernameException {
-        Pattern p = Pattern.compile("[^A-Za-z0-9]");
-        Matcher m = p.matcher(username);
-        if (m.find()) {
-            throw new InvalidUsernameException(username);
-        }
-        for (User u: mydrive.getUserSet()) {
-            if (u.getUsername() == username) {
-                throw new UserAlreadyExistsException(username);
-            }
-        }
-        Dir home = new Dir(mydrive, name, umask, this, mydrive.getRootDir());
-        setUsername(username);
-        setPassword(password);
-        setName(name);
-        setUmask(umask);
-        setDir(home);
-        setMydrive(mydrive);
-	
-    }
-    
-    public User(Dir home, MyDrive mydrive, Element xml) throws ImportDocumentException, NotFileException {
-        setDir(home);
-        setMydrive(mydrive);
-        xmlImport(xml);
-
-
-    }
-
     /**
      *  Super User special constructor
      */
-    public User(MyDrive mydrive, Dir parent) {
-        Dir dirRoot;
+    public User(MyDrive mydrive) {
         setUsername("root");
         setPassword("rootroot");
         setName("Super User");
         setUmask("rwxdr-x-");
         setMydrive(mydrive);
-        dirRoot = new Dir(mydrive, "root", "rwxdr-x-", this, parent);
-        setDir(dirRoot);
+        Login login = new Login("root", "rootroot");
+        setDir(new Dir(login, "root", "rwxdr-x-", this));
 	
     }
     
-    @Override
-    public String toString() {
-        return "Name:" + getName() + "Username:" + getUsername() + "Umask:" + getUmask();
+    /**
+     *  User constructor
+     *  @param  username    User's username
+     *  @param  password    User's password
+     *  @param  umask       User's mask
+     *  @param  mydrive     MyDrive application
+     */
+    public User(String username, String password, String name, String umask, Login login) throws UserAlreadyExistsException , InvalidUsernameException, PermissionDeniedException {
+        Pattern p = Pattern.compile("[^A-Za-z0-9]");
+        Matcher m = p.matcher(username);
+        if (!(login.getUser().equals(login.getMydriveL().getUserByUsername("root")))) {
+            throw new PermissionDeniedException();
+        }
+        if (m.find() || (username.length() < 3)) {
+            throw new InvalidUsernameException(username);
+        }
+        for (User u: login.getMydriveL().getUserSet()) {
+            if (u.getUsername() == username) {
+                throw new UserAlreadyExistsException(username);
+            }
+        }
+        Dir home = new Dir(login, name, umask, this);
+        setUsername(username);
+        setPassword(password);
+        setName(name);
+        setUmask(umask);
+        setDir(home);
+        setMydrive(login.getMydriveL());
+	
     }
     
+    /**
+     *  User XML constructor
+     *  @param  mydrive     MyDrive application
+     *  @param  xml	xml element
+     */
+    public User(MyDrive mydrive, Element xml) throws ImportDocumentException, NotFileException {
+        setMydrive(mydrive);
+        xmlImport(xml);
+    }
+    
+
     public void xmlImport(Element userElement) throws ImportDocumentException, NotFileException {
+        for (File f: getFileSet())
+            f.remove();
+
         try {
             setUsername(new String(userElement.getAttribute("username").getValue().getBytes("UTF-8")));
             setPassword(new String(userElement.getAttribute("password").getValue().getBytes("UTF-8")));
@@ -73,9 +84,25 @@ public class User extends User_Base {
         }   catch (UnsupportedEncodingException e) {
                 throw new ImportDocumentException();
             }
+
+        Element files = userElement.getChild("files");
+
+		// FIXME this.getDir()
+        for (Element dirElement: files.getChildren("dir"))
+            new Dir(this.getMydrive(),this, this.getDir(), dirElement);
+
+        for (Element plainfileElement: files.getChildren("plain-file"))
+            new PlainFile(this.getMydrive(), this, this.getDir(), plainfileElement);
+
+        for (Element appElement: files.getChildren("app"))
+            new App(this.getMydrive(),this, this.getDir(), appElement);
+
+        for (Element linkElement: files.getChildren("link"))
+            new Link(this.getMydrive(),this, this.getDir(), linkElement);
         
     }
-
+    
+/
     public Element xmlExport() {
         Element element = new Element("user");
         element.setAttribute("username", getUsername());
@@ -85,13 +112,27 @@ public class User extends User_Base {
         element.addContent(getDir().xmlExport());
         return element; 
     }
-
-	public void remove() {
+    
+    /**
+     *  User remover
+     *  @param  mydrive     MyDrive application
+     */
+    public void remove() {
         Dir home = getDir();
         for (File f: home.getFile()) {
             f.remove();
         }
         setMydrive(null);
         deleteDomainObject();
+    }
+    
+    /**
+     *  User's description
+     *  Example:
+     *  Name: user     Username: username      Umask: rwxdr-x-
+     */
+    @Override
+    public String toString() {
+        return "Name:" + getName() + "Username:" + getUsername() + "Umask:" + getUmask();
     }
 }
