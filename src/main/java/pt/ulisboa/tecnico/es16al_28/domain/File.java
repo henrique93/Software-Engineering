a0e4e6d0200db38ddb9810e5bef089b573850d82
pedrocruz27 <pedro.m.g.cruz@tecnico.ulisboa.pt>
2016-04-14 23:08:34 +0100

    File and Link updated in domain

--- src/main/java/pt/ulisboa/tecnico/es16al_28/domain/File.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/domain/File.java
@@ -1,18 +1,29 @@
 package pt.ulisboa.tecnico.es16al_28.domain;
 
 import org.jdom2.Element;
+import java.io.UnsupportedEncodingException;
+import pt.ulisboa.tecnico.es16al_28.exception.ImportDocumentException;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public abstract class File extends File_Base {
     
+    /**
+     *  Basic constructor
+     */
     public File() {
         super();
     }
     
     /**
-     *  file initializer
+     *  File initializer
+     *  @param  mydrive     MyDrive application
+     *  @param  name        File's name
+     *  @param  permission  File's permissions
+     *  @param  owner       User who owns this file (is either it's creator or the Super User)
+     *  @param  dir         Directory on which this file is located
      */
     public void init(MyDrive mydrive, String name, String permission, User owner, Dir dir) {
         setId(mydrive.incID());
@@ -21,15 +32,41 @@ public abstract class File extends File_Base {
         setLastChange(timeStamp);
         setPermission(permission);
         setOwner(owner);
-        setDir(dir);
+        setParent(dir);
+    }
+    /**
+     *  File initializer for xml
+     */
+    public void initxml(MyDrive mydrive, User owner, Dir dir, Element xml) throws ImportDocumentException {
+    	setId(mydrive.incID());
+    	setOwner(owner);
+        setParent(dir);
+        xmlImport(xml);
     }
 
-     public void remove() {
-      	setDir(null);
+     /**
+     *  File remover
+     */
+    public void remove() {
+      	setParent(null);
+        setOwner(null);
         deleteDomainObject();
     }
 
-     public Element xmlExport() {
+    
+    public void xmlImport(Element fileElement) throws ImportDocumentException {
+
+	try {
+            setName(new String(fileElement.getAttribute("name").getValue().getBytes("UTF-8")));
+            setPermission(new String(fileElement.getAttribute("permission").getValue().getBytes("UTF-8")));
+
+	} catch (UnsupportedEncodingException e) {
+            throw new ImportDocumentException();
+        }
+    }
+    
+    
+    public Element xmlExport() {
 	Element element = new Element("File");
     	element.setAttribute("id", Integer.toString(getId()));
         element.setAttribute("name", getName());
@@ -38,7 +75,20 @@ public abstract class File extends File_Base {
 	return element;
     }
 	
-    
+    public boolean isDir(){
+	return false;
+    }
+
+    public boolean isFile(){
+	return true;
+    }  
+
+
+    /**
+     *  File's description
+     *  Example:
+     *  "File ID: 21   Name: File  Owner: User     Permissions: rwxdr---   Modified last at: 2016.03.20.15.36.28"
+     */
     @Override
     public String toString() {
         return "File ID: " + getId() + "\tName: " + getName() + "\tOwner: " + getOwner().toString() + "\tPermisisons: " + getPermission() + "\tModified last at: " + getLastChange();
--- src/main/java/pt/ulisboa/tecnico/es16al_28/domain/Link.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/domain/Link.java
@@ -1,19 +1,47 @@
 package pt.ulisboa.tecnico.es16al_28.domain;
 
 import org.jdom2.Element;
+import java.io.UnsupportedEncodingException;
+
+import pt.ulisboa.tecnico.es16al_28.exception.ImportDocumentException;
 
 public class Link extends Link_Base {
     
-    public Link(MyDrive mydrive, String name, String permission, User owner, Dir dir, String app) {
+    /**
+     *  Link's constructor
+     *  @param  login       Current MyDrive login
+     *  @param  name        Link's name
+     *  @param  app         Link's content
+     */
+    public Link(Login login, String name, String app) {
         super();
-        init(mydrive, name, permission, owner, dir);
-	super.setApp(app);
+        init(login.getMydriveL(), name, login.getUser().getUmask(), login.getUser(), login.getCurrentDir());
+        setApp(app);
     }
+    
+    /**
+     *  Link's constructor for xml
+     *  @param  mydrive      Current MyDrive 
+     *  @param  owner        Link's owner
+     *  @param  dir          Link's dir
+     */
+    public Link(MyDrive mydrive, User owner, Dir dir, Element xml) throws ImportDocumentException {
+     	initxml(mydrive, owner, dir,  xml); 	
+    }
+    
+    
+    public void xmlImport(Element linkElement) throws ImportDocumentException {
 
+		super.xmlImport(linkElement);
+
+    }
+    
+   
     public Element xmlExport() {
         Element element = super.xmlExport();
-	element.setName("Link");
+	    element.setName("Link");
         return element; 
     }
     
 }
+
