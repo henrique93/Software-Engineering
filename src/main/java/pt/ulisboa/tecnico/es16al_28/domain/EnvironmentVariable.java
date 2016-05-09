4f749bda5c540430a0f426cfee6c1cc5a9b3626e
henrique93 <henrique.lourenco093@gmail.com>
2016-05-09 12:03:55 +0100

    EnvironmentVariable domain

--- src/main/java/pt/ulisboa/tecnico/es16al_28/domain/EnvironmentVariable.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/domain/EnvironmentVariable.java
@@ -1,9 +1,34 @@
 package pt.ulisboa.tecnico.es16al_28.domain;
 
+import pt.ulisboa.tecnico.es16al_28.exception.PermissionDeniedException;
+
 public class EnvironmentVariable extends EnvironmentVariable_Base {
     
-    public EnvironmentVariable() {
+    public EnvironmentVariable(Login login, String name, String value) {
         super();
+        super.setLogin(login);
+        super.setName(name);
+        super.setValue(value);
+    }
+
+    public void updateEv(String value) {
+        super.setValue(value);
+    }
+
+    /* OVERRIDE SETTERS FOR SECURITY */
+    @Override
+    public void setLogin(Login login) throws PermissionDeniedException {
+        throw new PermissionDeniedException();
+    }
+
+    @Override
+    public void setName(String name) throws PermissionDeniedException {
+        throw new PermissionDeniedException();
+    }
+
+    @Override
+    public void setValue(String value) throws PermissionDeniedException {
+        throw new PermissionDeniedException();
     }
     
 }
