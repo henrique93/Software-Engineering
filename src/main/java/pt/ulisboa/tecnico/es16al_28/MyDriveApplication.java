97fed7c21dd844fb63c9738e47b0d711e8901cef
henrique93 <henrique.lourenco093@gmail.com>
2016-05-12 20:39:45 +0100

    main cleared

--- src/main/java/pt/ulisboa/tecnico/es16al_28/MyDriveApplication.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/MyDriveApplication.java
@@ -35,8 +35,10 @@ public class MyDriveApplication {
     public static void main(String[] args) throws IOException {
         System.out.println("*** Welcome to the MyDrive application! ***");
         try {
+           /* for (String s: args) xmlScan(new File(s));
+            print();*/
 	    setup();
-            xmlPrint();
+            //xmlPrint();
 
         } finally {  System.out.println("*** Work done, shutting down ***");
 	FenixFramework.shutdown(); }
@@ -51,9 +53,45 @@ public class MyDriveApplication {
     public static void setup() { // MyDrive with debug data
         log.trace("Setup: " + FenixFramework.getDomainRoot());
         MyDrive app = MyDrive.getInstance();
-
+        /*User currentUser = app.getSuperUser();
+        Dir currentDir = app.getRootDir();
+        currentUser = new User("Alberto","lol", "Alberto", "rwxdr---", app);
+        try {
+            Login logged = new Login(app, "Alberto", "chumbartodos");
+        } catch (IncorrectPasswordException e) {System.out.println(e);};
+        currentDir = currentUser.getDir();
+        PlainFile plainFile = new PlainFile(app, "FICHAALBERTO", "rwxdrw-d", currentUser, currentDir, "sasdsa");
+        currentDir = new Dir(app, "Albertopasta", "rwxdr-x-", currentUser, currentDir);
+        currentDir = app.getRootDir();
+        Link link = new Link(app, "FICHAALBERTO", "rwxdrw-d", currentUser, currentDir, "asa");
+        currentUser = new User("AlbertoMaria","chumbartodos", "Alberto Maria", "rwxdr---", app);
+        currentDir = currentUser.getDir();
+        plainFile = new PlainFile(app, "TEST", "rwxdrw-d", currentUser, currentDir,"assadsad");
+        /*currentDir = currentDir.getParent();
+        currentDir = new Dir(app, fileID++, "Turi", "rwxdr-x-", "Turi", currentDir);
+        currentUser = new User("Turi", "1234", "tourette", "rwxdrw-d", currentDir, app);
+        currentDir = currentDir.getParent();
+        System.out.println(currentDir.listDir());
+        PlainFile plainFile = new PlainFile(app, fileID++, "TEST", "rwxdrw-d", currentUser, currentDir);
+        plainFile.setApp("FICHEIRO TESTE DE LEITURA DE CONTEUDO");
+        System.out.println(plainFile.readFile());
+        System.out.println(currentDir.listDir());
+        currentDir.rm(currentUser.getUsername(), plainFile);
+        System.out.println(currentDir.listDir());*/
     }
-
+/*    
+    @Atomic
+    public static void print() {
+        log.trace("Print: " + FenixFramework.getDomainRoot());
+        PhoneBook pb = PhoneBook.getInstance();
+        
+        for (Person p: pb.getPersonSet()) {
+            System.out.println("The Contact book of " + p.getName() + " contains " + p.getContactSet().size() + " contacts :");
+            for (Contact c: p.getContactSet())
+                System.out.println("\t" + c.getName() + " -> " + c.getPhoneNumber());
+        }
+    }
+    */
     @Atomic
     public static void xmlPrint() {
         log.trace("xmlPrint: " + FenixFramework.getDomainRoot());
@@ -62,4 +100,17 @@ public class MyDriveApplication {
         try { xmlOutput.output(doc, new PrintStream(System.out));
         } catch (IOException e) { System.out.println(e); }
     }
+   /*
+    @Atomic
+    public static void xmlScan(File file) {
+        log.trace("xmlScan: " + FenixFramework.getDomainRoot());
+        MyDrive app = MyDrive.getInstance();
+        SAXBuilder builder = new SAXBuilder();
+        try {
+            Document document = (Document)builder.build(file);
+            app.xmlImport(document.getRootElement());
+        } catch (JDOMException | IOException e) {
+            e.printStackTrace();
+        }
+    }*/
 }
