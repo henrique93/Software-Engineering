5ebf4f4c7f2bf7e83a385d5ee7c29f52fcfd9d6a
gus-gomes <gustavo_gomes94@hotmail.com>
2016-05-12 18:48:16 +0100

    List service update and list presentation done

--- src/main/java/pt/ulisboa/tecnico/es16al_28/service/ListDirectoryService.java
+++ src/main/java/pt/ulisboa/tecnico/es16al_28/service/ListDirectoryService.java
@@ -1,12 +1,13 @@
- package pt.ulisboa.tecnico.es16al_28.service;
+package pt.ulisboa.tecnico.es16al_28.service;
+
+import java.util.*;
 
 import pt.ulisboa.tecnico.es16al_28.exception.TokenDoesNotExistException;
 import pt.ulisboa.tecnico.es16al_28.exception.PermissionDeniedException;
 import pt.ulisboa.tecnico.es16al_28.domain.Login;
 import pt.ulisboa.tecnico.es16al_28.domain.MyDrive;
 import pt.ulisboa.tecnico.es16al_28.domain.File;
-import pt.ulisboa.tecnico.es16al_28.domain.Dir;
-import java.util.*;
+import pt.ulisboa.tecnico.es16al_28.domain.User;
 import pt.ulisboa.tecnico.es16al_28.service.dto.FileDto;
 
 public class ListDirectoryService extends MyDriveService {
@@ -20,31 +21,19 @@ public class ListDirectoryService extends MyDriveService {
 
 
     @Override
-    public final void dispatch() throws TokenDoesNotExistException, PermissionDeniedException{
+    public final void dispatch() throws TokenDoesNotExistException, PermissionDeniedException {
         MyDrive mydrive = getMyDrive();
         _listDirectory  = new ArrayList<FileDto>();
         Login l = mydrive.getLoginByToken(_token);
-        
-        Dir d = l.getCurrentDir();
-        Dir p = d.getParent();
-        
-        if (d.listPermission(l) || p.listPermission(l)){
-
-            FileDto Dto = new FileDto(d.getId(), d.getName(), d.getOwner().getUsername(), d.getPermission(), d.getLastChange());
+        ArrayList<File> list = l.ls();
+        for (File f: list) {
+            User owner = f.getOwner();
+            FileDto Dto = new FileDto(f.getId(), f.getName(), owner.getUsername(), f.getPermission(), f.getLastChange());
             _listDirectory.add(Dto);
-            Dto = new FileDto(p.getId(), p.getName(), p.getOwner().getUsername(), p.getPermission(), p.getLastChange());
-            _listDirectory.add(Dto);
-        
-            for (File f : d.getFileSet()){
-                Dto = new FileDto(f.getId(), f.getName(), f.getOwner().getUsername(), f.getPermission(), f.getLastChange());
-                _listDirectory.add(Dto);
-            }
-        }else{
-            throw new PermissionDeniedException();
         }
     }
     
-    public final List<FileDto> result(){
+    public final List<FileDto> result() {
         return _listDirectory;
     }
 
